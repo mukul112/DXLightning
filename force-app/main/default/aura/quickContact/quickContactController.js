666fc59b9df07fbe50f0d27196da38ed0a29efb1
mukul112 <mukulmishra112@gmail.com>
2017-09-05 16:25:19 +0530

    changes done

---
+++ force-app/main/default/aura/quickContact/quickContactController.js
@@ -0,0 +1,10 @@
+({
+    // function to handle the "Save" button click
+    handleSave : function(component, event, helper) {
+        //invoke Data Service's inbuilt saveRecord function
+        component.find("DataService").saveRecord(function(result){});
+    }
+})
+
+
+
